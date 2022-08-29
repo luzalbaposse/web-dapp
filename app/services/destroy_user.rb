cb65381af6ba061ab30a0f0d6987ba6e63af0ac0
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2022-08-29 13:46:53 +0100

    Delete user chats on user destroy

--- app/services/destroy_user.rb
+++ app/services/destroy_user.rb
@@ -46,6 +46,8 @@ class DestroyUser
       Transfer.where(user: user).update_all(user_id: nil)
       Message.where(sender_id: user.id).destroy_all
       Message.where(receiver_id: user.id).destroy_all
+      Chat.where(sender_id: user.id).destroy_all
+      Chat.where(receiver_id: user.id).destroy_all
       Quest.where(user: user).destroy_all
       UserTag.where(user: user).destroy_all
       UserProfileTypeChange.where(user: user).destroy_all
