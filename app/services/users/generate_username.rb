02e86aa2ab98cf2ec8f706c187fe677747f4f3d8
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2022-11-09 16:46:51 +0000

    Return random sequence for username when it's empty

--- app/services/users/generate_username.rb
+++ app/services/users/generate_username.rb
@@ -6,13 +6,19 @@ module Users
 
     def call
       parameterized_name = display_name.parameterize(separator: "")
+      return random_sequence.to_s if parameterized_name.blank?
+
       return parameterized_name unless User.find_by(username: parameterized_name).present?
 
-      "#{parameterized_name}#{(SecureRandom.random_number(9e5) + 1e5).to_i}"
+      "#{parameterized_name}#{random_sequence}"
     end
 
     private
 
     attr_reader :display_name
+
+    def random_sequence
+      @random_sequence ||= (SecureRandom.random_number(9e5) + 1e5).to_i
+    end
   end
 end
