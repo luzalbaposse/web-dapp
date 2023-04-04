98af336ca8d659c4c4cde9b815086269c170e734
bguedes <bguedesdev@gmail.com>
2023-04-04 09:32:41 +0100

    feat: added career update input on empty state (#1037)

--- app/packs/src/components-v2/pages/talent-models-profile/career-updates/empty-state/index.jsx
+++ app/packs/src/components-v2/pages/talent-models-profile/career-updates/empty-state/index.jsx
@@ -1,8 +1,11 @@
 import { Icon, Typography, Button } from "@talentprotocol/design-system";
-import React from "react";
+import React, { useState } from "react";
 import { Container, TextColumn } from "./styled";
+import SendCareerUpdateModal from "../../../../../components/profile/SendCareerUpdateModal";
+import TextInput from "src/components/design_system/fields/textinput";
 
-export const CareerUpdateEmptyState = ({ profile }) => {
+export const CareerUpdateEmptyState = ({ profile, isCurrentUserProfile, railsContext }) => {
+  const [isSendCareerUpdateModalOpen, setShowCareerUpdateModal] = useState(false);
   return (
     <Container>
       <Icon name="mailbox" size={48} color="primary04" />
@@ -10,17 +13,34 @@ export const CareerUpdateEmptyState = ({ profile }) => {
         <Typography specs={{ variant: "h5", type: "bold" }} color="primary01">
           {profile.user.name} doesn't have career updates
         </Typography>
-        <Typography specs={{ variant: "p2", type: "regular" }} color="primary01">
-          Ask for an update
-        </Typography>
+        {isCurrentUserProfile ? (
+          <TextInput
+            placeholder={`What's new in your career ${profile.user.name}?`}
+            onClick={() => setShowCareerUpdateModal(true)}
+            className="w-100 mt-3"
+          />
+        ) : (
+          <Typography specs={{ variant: "p2", type: "regular" }} color="primary01">
+            Ask for an update
+          </Typography>
+        )}
       </TextColumn>
-      <Button
-        hierarchy="primary"
-        variant=""
-        size="medium"
-        text="Send message"
-        href={`/messages?user=${profile.user.username}`}
-      />
+      {isCurrentUserProfile ? (
+          <SendCareerUpdateModal
+            show={isSendCareerUpdateModalOpen}
+            hide={() => setShowCareerUpdateModal(false)}
+            placeholder={`What's new in your career ${profile.user.name}?`}
+            contractsEnv={railsContext}
+          />
+      ) : (
+        <Button
+          hierarchy="primary"
+          variant=""
+          size="medium"
+          text="Send message"
+          href={`/messages?user=${profile.user.username}`}
+        />
+      )}
     </Container>
   );
 };
