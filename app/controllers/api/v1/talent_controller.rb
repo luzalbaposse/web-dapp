a826f81156d6d722cdcc02c027f631fde15c8062
Rúben Sousa Dinis <Ruben.sousa.dinis@gmail.com>
2023-04-04 17:06:37 +0100

    Allow non logged in users to access discovery pages (#1046)

--- app/controllers/api/v1/talent_controller.rb
+++ app/controllers/api/v1/talent_controller.rb
@@ -5,7 +5,7 @@ class API::V1::TalentController < ApplicationController
   def index
     paging, talents = Talents::ChewySearch.new(
       filter_params: filter_params.to_h,
-      admin_or_moderator: current_user.admin_or_moderator?,
+      admin_or_moderator: current_user&.admin_or_moderator?,
       size: per_page,
       from: ((params[:page] || PAGE_NEUTRALIZER).to_i - PAGE_NEUTRALIZER) * per_page,
       searching_user: current_user,
