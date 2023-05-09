c0133a33d829f808677257dea4967f5b3e87671a
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2023-05-09 10:38:37 +0100

    Encode email param before validation

--- app/packs/src/api/validations.js
+++ app/packs/src/api/validations.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const validateHandle = localHandle => axios.get(`/api/v1/validations/username?username=${localHandle}`);
 
-const validateEmail = email => axios.get(`/api/v1/validations/email?email=${email}`);
+const validateEmail = email => axios.get(`/api/v1/validations/email?email=${encodeURIComponent(email)}`);
 
 export const validations = {
   validateHandle,
