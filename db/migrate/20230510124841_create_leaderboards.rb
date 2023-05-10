ffdc33b3e275c8d5a237c759786f989656239958
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2023-05-10 15:03:24 +0100

    Add uuid to leaderboard

--- db/migrate/20230510124841_create_leaderboards.rb
+++ db/migrate/20230510124841_create_leaderboards.rb
@@ -1,6 +1,7 @@
 class CreateLeaderboards < ActiveRecord::Migration[7.0]
   def change
     create_table :leaderboards do |t|
+      t.uuid :uuid, default: "gen_random_uuid()", index: true, null: false, unique: true
       t.references :race, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
       t.integer :score, null: false, default: 0
