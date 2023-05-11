3a8e3679465e34136cf71ae8bf76f09f812ddf63
francisco-leal <francisco@talentprotocol.com>
2023-05-11 09:31:10 +0100

    Update activity creation

--- app/models/activities/token_launch.rb
+++ app/models/activities/token_launch.rb
@@ -7,9 +7,7 @@ module Activities
     def self.generate_content(origin_user_id, _)
       origin_user = User.find(origin_user_id)
 
-      message = "#{origin_user.name} just launched their token $#{origin_user.talent.talent_token.ticker}!"
-
-      JSON.generate(message: message)
+      "#{origin_user.name} just launched their token $#{origin_user.talent.talent_token.ticker}!"
     end
   end
 end
