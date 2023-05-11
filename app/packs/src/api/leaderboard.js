b207b5719981816fa7cd8dd2ebe7ae13aea2de6a
helloiambguedes <bguedesdev@gmail.com>
2023-05-11 10:25:46 +0100

    changed leadderboards endpoint

--- app/packs/src/api/leaderboard.js
+++ app/packs/src/api/leaderboard.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const getLeaderboard = () => axios.get(`/api/v1/leaderboard`);
+const getLeaderboard = () => axios.get(`/api/v1/leaderboards`);
 
 export const leaderboardService = {
   getLeaderboard
