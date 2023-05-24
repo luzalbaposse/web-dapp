43bb70d13c6ae422aa284ba717681063a7a03c10
Rúben Sousa Dinis <Ruben.sousa.dinis@gmail.com>
2023-05-24 14:16:03 +0100

    Fix duplicated quests loading issue (#1231)

--- app/controllers/api/v1/public_api/quests_controller.rb
+++ app/controllers/api/v1/public_api/quests_controller.rb
@@ -3,9 +3,12 @@ class API::V1::PublicAPI::QuestsController < API::V1::PublicAPI::APIController
     total_quests = V2Quest.all
 
     if user
-      total_quests = total_quests
-        .left_joins(:user_v2_quests)
-        .where(user_v2_quests: {user_id: u.id})
+      total_quests = total_quests.joins(
+        "
+          LEFT JOIN user_v2_quests on user_v2_quests.v2_quest_id = v2_quests.id
+          AND user_v2_quests.user_id = #{user.id}
+        "
+      )
     end
 
     pagy, quests = pagy_uuid_cursor(
@@ -15,8 +18,10 @@ class API::V1::PublicAPI::QuestsController < API::V1::PublicAPI::APIController
       order: {experience_points_amount: :asc, uuid: :desc}
     )
 
+    quests = quests.select("v2_quests.*, user_v2_quests.completed_at") if user
+
     response_body = {
-      quests: API::QuestBlueprint.render_as_json(quests.left_joins(:user_v2_quests).select("v2_quests.*, user_v2_quests.completed_at"), view: :normal),
+      quests: API::QuestBlueprint.render_as_json(quests, view: :normal),
       pagination: {
         total: total_quests.count,
         cursor: pagy.has_more? ? quests.last.uuid : nil
