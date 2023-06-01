ca9716588f9cc8ec059e1cd1c39ef854fac6301f
Ruben Dinis <ruben.sousa.dinis@gmail.com>
2023-06-01 17:20:34 +0100

    Remove unused code

--- app/controllers/talent_controller.rb
+++ app/controllers/talent_controller.rb
@@ -1,17 +1,4 @@
 class TalentController < ApplicationController
-  PER_PAGE = 40
-  PAGE_NEUTRALIZER = 1
-
   def index
-  end
-
-  private
-
-  def filter_params
-    params.permit(:keyword, :status)
-  end
-
-  def per_page
-    (params[:per_page] || PER_PAGE).to_i
   end
 end
