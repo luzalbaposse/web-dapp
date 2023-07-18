c767106fb3b2adc721d935a057b756a398a008a1
helloiambguedes <bguedesdev@gmail.com>
2023-07-18 18:01:35 +0100

    ui: addded desktop layout

--- app/packs/src/components-v2/pages/profile/index.js
+++ app/packs/src/components-v2/pages/profile/index.js
@@ -1,15 +1,28 @@
 import React from "react";
-import { PageContainer } from "./styled";
+import isMobile from "is-mobile";
+import { PageContainer, DesktopPageContainer, DesktopColumn } from "./styled";
 import { ProfileHeader } from "./profile-header";
 
-export const ProfilePage = () => {
-  return (
+export const ProfilePage = ({ isMobile }) => {
+  return isMobile ? (
     <PageContainer>
       <ProfileHeader />
     </PageContainer>
-  );
+  ) : (
+    <DesktopPageContainer>
+      <DesktopColumn>
+        <ProfileHeader />
+      </DesktopColumn>
+      <DesktopColumn>
+
+      </DesktopColumn>
+      <DesktopColumn>
+
+      </DesktopColumn>
+    </DesktopPageContainer>
+  )
 }
 
 export default (props, railsContext) => {
-  return () => <ProfilePage {...props} railsContext={railsContext} />;
+  return () => <ProfilePage {...props} railsContext={railsContext} isMobile={isMobile()} />;
 };
--- app/packs/src/components-v2/pages/profile/styled.js
+++ app/packs/src/components-v2/pages/profile/styled.js
@@ -1,5 +1,34 @@
+import { buildColor } from "@talentprotocol/design-system";
 import styled from "styled-components";
 
 export const PageContainer = styled.div`
-    padding-top: 64px;
+  padding-top: 64px;
+`;
+
+export const DesktopPageContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  padding-top: 68px;
+  max-width: 1224px;
+  margin: auto;
+`;
+
+export const DesktopColumn = styled.div`
+  display: flex;
+  border-right: 1px solid ${buildColor("surfaceHover02")};
+
+  :nth-child(1) {
+    flex-basis: 288px;
+    padding: 24px 24px 0 0;
+  }
+
+  :nth-child(2) {
+    flex-grow: 1;
+  }
+
+  :nth-child(3) {
+    flex-basis: 288px;
+    padding: 24px 0 0 24px;
+    border: none;
+  }
 `;
