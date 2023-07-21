05ca8b38e4aa1e940ff71832d3d00ecf59d77ce8
helloiambguedes <bguedesdev@gmail.com>
2023-07-21 12:38:03 +0100

    added spinners to about and support tabs

--- app/packs/src/components-v2/pages/profile/profile-areas/about/index.jsx
+++ app/packs/src/components-v2/pages/profile/profile-areas/about/index.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
+import { Spinner } from "@talentprotocol/design-system";
 import { useProfileFetcher } from "src/hooks/use-profile-fetcher";
 import OldAbout from "../../../../../components/profile/About";
 import OldJourney from "../../../../../components/profile/Journey";
+import { SpinnerContainer } from "./styled";
 
 export const About = ({ currentUser, urlData }) => {
   const { profile, fetchProfile } = useProfileFetcher();
@@ -9,14 +11,14 @@ export const About = ({ currentUser, urlData }) => {
     fetchProfile(urlData.profileUsername);
   }, []);
 
-
-  // todo: add spinner
-  return (
-    !!profile && (
-      <>
-        <OldAbout profile={profile} />
-        <OldJourney talent={profile} canUpdate={profile.user.username === currentUser.username} />
-      </>
-    )
+  return !profile ? (
+    <SpinnerContainer>
+      <Spinner color="primary" size={48} />
+    </SpinnerContainer>
+  ) : (
+    <>
+      <OldAbout profile={profile} />
+      <OldJourney talent={profile} canUpdate={profile.user.username === currentUser.username} />
+    </>
   );
 };
--- app/packs/src/components-v2/pages/profile/profile-areas/support/index.jsx
+++ app/packs/src/components-v2/pages/profile/profile-areas/support/index.jsx
@@ -1,28 +1,31 @@
 import React, { useEffect } from "react";
 import { noop } from "lodash";
+import { Spinner } from "@talentprotocol/design-system";
 import { useProfileFetcher } from "src/hooks/use-profile-fetcher";
 import { Models } from "../../../talent-models-profile/models";
 import Token from "../../../../../components/profile/Token";
+import { SpinnerContainer } from "./styled";
 
 export const Support = ({ currentUser, railsContext, urlData }) => {
   const { profile, fetchProfile } = useProfileFetcher();
   useEffect(() => {
     fetchProfile(urlData.profileUsername);
   }, []);
-  // todo: add spinner
-  return (
-    !!profile && (
-      <>
-        <Models
-          profile={profile}
-          setProfile={noop}
-          currentUserId={currentUser?.id}
-          isCurrentUserProfile={profile.user.username === currentUser?.username}
-          railsContext={railsContext}
-        />
-        {/* ask this out */}
-        <Token profile={profile} talentTokenPrice={0.1} railsContext={railsContext} />
-      </>
-    )
+  return !profile ? (
+    <SpinnerContainer>
+      <Spinner color="primary" size={48} />
+    </SpinnerContainer>
+  ) : (
+    <>
+      <Models
+        profile={profile}
+        setProfile={noop}
+        currentUserId={currentUser?.id}
+        isCurrentUserProfile={profile.user.username === currentUser?.username}
+        railsContext={railsContext}
+      />
+      {/* ask this out */}
+      <Token profile={profile} talentTokenPrice={0.1} railsContext={railsContext} />
+    </>
   );
 };
