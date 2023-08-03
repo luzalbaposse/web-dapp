0c9976481a071d3f1e676503bef031a7cfc3a36b
helloiambguedes <bguedesdev@gmail.com>
2023-08-03 15:17:45 +0100

    fix: fixed typo

--- ci-tests-helper.bash
+++ ci-tests-helper.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LAST_COMMIT=$(git rev-parse HEAD)
-FILES=$(git show --pretty="" --name-only 08532ddda4dce1cca98dbb0e932cbca9aed51e56 | grep js)
+FILES=$(git show --pretty="" --name-only $LAST_COMMIT | grep js)
 
 if [ -z "$FILES" ]; then
     echo "No changes affecting tests"
