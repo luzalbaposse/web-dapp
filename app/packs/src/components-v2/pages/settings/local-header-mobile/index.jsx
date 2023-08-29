7ba547b53a4c2c5846126b6a81b96b3f83d25573
helloiambguedes <bguedesdev@gmail.com>
2023-08-29 16:17:09 +0100

    fix: mobile header fully working

--- app/packs/src/components-v2/pages/settings/local-header-mobile/index.jsx
+++ app/packs/src/components-v2/pages/settings/local-header-mobile/index.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { noop } from "lodash";
 import { TextLink } from "@talentprotocol/design-system";
 import { HamburguerContainer, HeaderContainer, InnerHeaderContainer, StyledNavLinks } from "./styled";
 
-export const LocalHeaderMobile = ({ username, goToPage, openHamburguer, isHamburguerOpen }) => {
+export const LocalHeaderMobile = ({ username, goToPage, openHamburguer, isHamburguerOpen, page }) => {
   return (
     <>
       <HeaderContainer>
         <InnerHeaderContainer>
           <TextLink
             color="primary01"
-            text={"Edit Profile"}
+            text={isHamburguerOpen ? "Edit Profile" : page}
             leftIcon="back-arrow"
+            href={`/u/${username}`}
             size="small"
             onClick={openHamburguer}
           />
