56478169a584eae09784f6264052d6a5e12a94ce
Rúben Sousa Dinis <Ruben.sousa.dinis@gmail.com>
2023-09-04 15:54:43 +0100

    Remove persona ip validation (#1429)

--- app/controllers/external/persona_webhooks_controller.rb
+++ app/controllers/external/persona_webhooks_controller.rb
@@ -1,12 +1,5 @@
 class External::PersonaWebhooksController < ApplicationController
-  VALID_IP_ADDRESSES = [
-    "35.232.44.140",
-    "34.69.131.123",
-    "34.67.4.225"
-  ]
-
   def create
-    return render json: {message: "Invalid IP address"}, status: :unauthorized unless valid_ip?
     return render json: {message: "Invalid Signature address"}, status: :unauthorized unless valid_signature?
 
     PersonaWebhookReceivedJob.perform_later(params: webhook_params.to_h)
@@ -15,10 +8,6 @@ class External::PersonaWebhooksController < ApplicationController
   end
 
   private
-
-  def valid_ip?
-    VALID_IP_ADDRESSES.include?(request.remote_ip)
-  end
 
   def valid_signature?
     t, v1 = request.headers["HTTP_PERSONA_SIGNATURE"].split(",").map { |value| value.split("=").second }
--- spec/requests/external/persona_webhooks_spec.rb
+++ spec/requests/external/persona_webhooks_spec.rb
@@ -16,40 +16,25 @@ RSpec.describe "Persona Webhooks", type: :request do
 
     before do
       ENV["WITH_PERSONA_WEBHOOK_SECRET"] = "secret"
-      allow_any_instance_of(ActionDispatch::Request).to receive(:remote_ip).and_return(request_ip)
       allow(OpenSSL::HMAC).to receive(:hexdigest).and_return("valid!")
     end
 
-    context "when a valid ip is doing the request" do
-      let(:request_ip) { "35.232.44.140" }
+    it "returns a successful response" do
+      persona_webhook_request
 
-      it "returns a successful response" do
-        persona_webhook_request
-
-        expect(response).to have_http_status(:ok)
-      end
-
-      it "starts a job to take care of the information received" do
-        persona_webhook_request
-
-        expect(PersonaWebhookReceivedJob).to have_been_enqueued
-      end
-
-      context "when the request signature is invalid" do
-        before do
-          allow(OpenSSL::HMAC).to receive(:hexdigest).and_return("invalid!")
-        end
-
-        it "returns an unauthorized response" do
-          persona_webhook_request
-
-          expect(response).to have_http_status(:unauthorized)
-        end
-      end
+      expect(response).to have_http_status(:ok)
     end
 
-    context "when an invalid ip is doing the request" do
-      let(:request_ip) { "35.232.34857.39" }
+    it "starts a job to take care of the information received" do
+      persona_webhook_request
+
+      expect(PersonaWebhookReceivedJob).to have_been_enqueued
+    end
+
+    context "when the request signature is invalid" do
+      before do
+        allow(OpenSSL::HMAC).to receive(:hexdigest).and_return("invalid!")
+      end
 
       it "returns an unauthorized response" do
         persona_webhook_request
