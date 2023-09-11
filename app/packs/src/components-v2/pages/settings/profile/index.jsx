23dfe73d36042252ed0259e617b647399807686b
helloiambguedes <bguedesdev@gmail.com>
2023-09-11 15:41:42 +0100

    upload image

--- app/packs/src/components-v2/pages/settings/profile/index.jsx
+++ app/packs/src/components-v2/pages/settings/profile/index.jsx
@@ -1,31 +1,47 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { toast } from "react-toastify";
+import Uppy from "@uppy/core";
+import { FileInput } from "@uppy/react";
+import AwsS3Multipart from "@uppy/aws-s3-multipart";
 import { useEditProfileStore } from "src/contexts/state";
 import { Container, AvatarRow, UploadContainer, UploadButtons, InfoRow } from "./styled";
 import { Avatar, Button, Input, TextArea, Typography } from "@talentprotocol/design-system";
 import { editProfileService } from "src/api/edit-profile";
 import { ToastBody } from "src/components/design_system/toasts";
+import { getAuthToken } from "src/api/utils";
 
 export const ProfileForm = () => {
+  const profileFileInput = useRef(null);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     setIsLoading(false);
   }, [setIsLoading]);
   const { profile, updateProfileState } = useEditProfileStore();
+  const [profilePic, setProfilePic] = useState({ url: profile?.profile_picture_url });
+  useEffect(() => {
+    if (!profile) return;
+    setProfilePic({url: profile.profile_picture_url});
+  }, [profile]);
   const updateProfile = useCallback(() => {
-    editProfileService
-      .editProfile(profile.username, {
-        user: {
-          display_name: refs.name.current.value
-        },
-        talent: {
-          profile: {
-            location: refs.location.current.value,
-            headline: refs.headline.current.value
-          }
+    const payloadObject = {
+      user: {
+        display_name: refs.name.current.value
+      },
+      talent: {
+        profile: {
+          location: refs.location.current.value,
+          headline: refs.headline.current.value,
+          profile_picture_url: profilePic
         }
-      })
+      }
+    };
+    if (profilePic.file) {
+      payloadObject.talent["profile_picture_data"] = profilePic.file;
+      payloadObject["profile_picture_url"] = profilePic.url;
+    }
+    editProfileService
+      .editProfile(profile.username, payloadObject)
       .then(() => {
         updateProfileState({
           name: refs.name.current.value,
@@ -39,29 +55,98 @@ export const ProfileForm = () => {
             location: refs.location.current.value,
             headline: refs.headline.current.value
           },
-          headline: refs.headline.current.value
+          headline: refs.headline.current.value,
+          profile_picture_url: profilePic.url
         });
       })
       .catch(err => {
         console.error(err);
         toast.error(<ToastBody heading="Error" body={"Something happened while updating your profile"} />);
       });
-  }, [profile]);
+  }, [profile, profilePic]);
   const refs = {
     name: useRef(),
     location: useRef(),
     headline: useRef()
   };
+  const uppyProfile = new Uppy({
+    meta: { type: "avatar" },
+    restrictions: {
+      maxFileSize: 5120000,
+      allowedFileTypes: [".jpg", ".png", ".jpeg"]
+    },
+    autoProceed: true
+  });
+  uppyProfile.use(AwsS3Multipart, {
+    limit: 4,
+    companionUrl: "/",
+    companionHeaders: {
+      "X-CSRF-Token": getAuthToken()
+    }
+  });
+  useEffect(() => {
+    if (profileFileInput.current) return;
+    profileFileInput?.current.addEventListener("change", event => {
+      const files = Array.from(event.target.files);
+      files.forEach(file => {
+        try {
+          uppyProfile.addFile({
+            source: "file input",
+            name: file.name,
+            type: file.type,
+            data: file
+          });
+        } catch (err) {
+          if (err.isRestriction) {
+            console.error("Restriction error:", err);
+            toast.error(<ToastBody heading="Error" body={"There is a file type restriction."} />);
+          } else {
+            console.error(err);
+            toast.error(<ToastBody heading="Error" body={"Something happened while uploading your avatar picture."} />);
+          }
+        }
+      });
+    });
+  }, [profileFileInput]);
+  useEffect(() => {
+    uppyProfile.on("restriction-failed", () => {
+      uppyProfile.reset();
+    });
+    uppyProfile.on("upload-success", (file, response) => {
+      setProfilePic({file:  {
+          // eslint-disable-next-line  no-useless-escape
+          id: response.uploadURL.match(/\/cache\/([^\?]+)/)[1], // extract key without prefix
+          new_upload: true,
+          storage: "cache",
+          metadata: {
+            size: file.size,
+            filename: file.name,
+            mime_type: file.type
+          }
+        }, url: response.uploadURL });
+    });
+    uppyProfile.on("upload", () => {});
+  }, [uppyProfile]);
   return (
     <Container>
       <AvatarRow>
-        <Avatar size="xl" url={profile?.profile_picture_url} />
+        <Avatar size="xl" url={profilePic.url} />
         <UploadContainer>
           <Typography specs={{ type: "regular", variant: "p3" }} color="primary04">
             JPG or PNG. Max 1MB
           </Typography>
           <UploadButtons>
-            <Button hierarchy="primary" size="small" text="Upload Profile Picture" />
+            <Button hierarchy="primary" size="small" text="Upload Profile Picture" buttonRef={profileFileInput} />
+            <FileInput
+                uppy={uppyProfile}
+                pretty
+                inputName="banners[]"
+                locale={{
+                  strings: {
+                    chooseFiles: "Upload Profile Picture",
+                  }
+                }}
+              />
           </UploadButtons>
         </UploadContainer>
       </AvatarRow>
