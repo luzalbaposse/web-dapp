b839a3e31c59af70ea6f597e640b704d886061f7
helloiambguedes <bguedesdev@gmail.com>
2023-09-12 11:54:07 +0100

    deleted unused file

--- app/packs/src/components-v2/pages/settings/local-header-mobile/use-mobile-header.js
+++
@@ -1,7 +0,0 @@
-import { BASE_PAGES } from "../base-pages";
-
-export const useMobileHeader = (initialPage = "profile") => {
-  const [currentPage, setCurrentPage] = useState(() => BASE_PAGES[initialPage]);
-  const goToPage = page;
-  return {};
-};
