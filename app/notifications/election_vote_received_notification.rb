511730da8c36aca922c790b9967d8ffad0adee1e
Luz Alba Posse <66437312+luzalbaposse@users.noreply.github.com>
2023-10-02 15:45:14 -0300

    Update election_vote_received_notification.rb
    feat: Enhance ElectionVoteReceivedNotification
    
    - Added functionality to display the total number of received votes.
    - Included candidate's position in the notification message.

--- app/notifications/election_vote_received_notification.rb
+++ app/notifications/election_vote_received_notification.rb
@@ -6,9 +6,19 @@ class ElectionVoteReceivedNotification < BaseNotification
     if: :should_deliver_immediate_email?
 
   def body
-    "You have received #{source.amount} votes from #{source.voter.name}"
+    "You have received #{source.amount} votes from #{source.voter.name}. Total votes received: #{total_votes_received}. Your position is: #{candidate_position}°"
   end
-
+  
+  def total_votes_received
+    Vote.where(election_id: params["election_id"]).sum(:amount)
+  end
+  
+  def calculate_candidate_position(candidate_id, election_id)
+    votes_received = Vote.where(election_id: election_id).order(amount: :desc).pluck(:candidate_id)
+    candidate_position = votes_received.index(candidate_id) + 1
+    candidate_position
+  end
+  
   def election
     Election.find_by(id: params["election_id"])
   end
